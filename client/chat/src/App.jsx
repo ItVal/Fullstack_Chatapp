0b164b3bff2d2963f8beb710b030992ce4f02128
ItVal <valentinhonvj1@gmail.com>
2022-11-07 16:45:29 +0100

    redirect room

--- client/chat/src/App.jsx
+++ client/chat/src/App.jsx
@@ -1,13 +1,31 @@
 import './App.css';
+import { useState } from 'react'; // Add this
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import io from 'socket.io-client'; // Add this
 import Home from './pages/home';
 
+const socket = io.connect('http://localhost:4000'); // Add this -- our server will run on port 4000, so we connect to it from here
+
 function App() {
+  const [username, setUsername] = useState(''); // Add this
+  const [room, setRoom] = useState(''); // Add this
+
   return (
     <Router>
       <div className='App'>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route
+            path='/'
+            element={
+              <Home
+                username={username} // Add this
+                setUsername={setUsername} // Add this
+                room={room} // Add this
+                setRoom={setRoom} // Add this
+                socket={socket} // Add this
+              />
+            }
+          />
         </Routes>
       </div>
     </Router>
