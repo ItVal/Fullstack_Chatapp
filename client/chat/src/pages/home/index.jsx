c19932e23f93c04f8fa8176c94f2a4eb006ba68e
ItVal <valentinhonvj1@gmail.com>
2022-11-07 16:04:06 +0100

    join a room

---
+++ client/chat/src/pages/home/index.jsx
@@ -0,0 +1,24 @@
+import styles from './styles.module.css';
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.formContainer}>
+        <h1>{`<>DevRooms</>`}</h1>
+        <input className={styles.input} placeholder='Username...' />
+
+        <select className={styles.input}>
+          <option>-- Select Room --</option>
+          <option value='javascript'>JavaScript</option>
+          <option value='node'>Node</option>
+          <option value='express'>Express</option>
+          <option value='react'>React</option>
+        </select>
+
+        <button className='btn btn-secondary'>Join Room</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
