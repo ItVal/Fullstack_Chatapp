7c2bb8393bfa53027ffb9809bb60bd770ee485c1
ItVal <valentinhonvj1@gmail.com>
2022-11-08 09:41:23 +0100

    connection client with server

--- client/chat/src/App.jsx
+++ client/chat/src/App.jsx
@@ -1,35 +1,14 @@
-import './App.css';
-import { useState } from 'react'; // Add this
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import io from 'socket.io-client'; // Add this
-import Home from './pages/home';
-
-const socket = io.connect('http://localhost:4000'); // Add this -- our server will run on port 4000, so we connect to it from here
+import React from "react";
+import "./App.css";
+import socketClient from "socket.io-client";
+const SERVER = "http://127.0.0.1:2080";
 
 function App() {
-  const [username, setUsername] = useState(''); // Add this
-  const [room, setRoom] = useState(''); // Add this
-
-  return (
-    <Router>
-      <div className='App'>
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <Home
-                username={username} // Add this
-                setUsername={setUsername} // Add this
-                room={room} // Add this
-                setRoom={setRoom} // Add this
-                socket={socket} // Add this
-              />
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
-  );
+  let socket = socketClient(SERVER);
+  socket.on('connection', () => {
+      console.log(`Je suis connecté avec le backend`);
+  });
+  return <div className="App">Bonjour chat app</div>;
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const cors = require('cors');
-const PORT = 2022;
+const http = require("http").createServer(app);
+const PORT = 2080;
+let socket = require("socket.io");
+const cors = require("cors");
 
-app.use(cors()); // Add cors middleware
+app.use(cors());
 
-const server = http.createServer(app);
+const STATIC_CHANNELS = ["global_notifications", "global_chat"];
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
+//authorisation accès aux requêtes venant de l'extérieur
+
+
+
+const io = socket(http, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  /* notification pour un client connecter */
+  console.log("new client connected");
+  socket.emit("connection", null); //evenement personnalisé du back vers le font
+});
+
+
+
+
+
+
+http.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
   });
-
-server.listen(2022, () => console.log("Server is running on port" + PORT));
