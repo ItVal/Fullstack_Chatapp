265691dc2b6e2ef8e493dd05e46b12ed8eefb72d
ItVal <valentinhonvj1@gmail.com>
2022-12-02 12:34:10 +0100

    Revert "declare environement variables"
    This reverts commit d2fd15fad6578048a0747ec7bae9ed7406fbdb98.

--- client/chat/src/Pages/ChatPage.jsx
+++ client/chat/src/Pages/ChatPage.jsx
@@ -181,7 +181,7 @@ const ChatPage = ({ socket }) => {
                   >
                     <img
                       class="object-cover w-10 h-10 rounded-full"
-                      src="https://api.lorem.space/image/face?w=150&h=150"
+                      src="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
                       alt="username"
                     />
 
@@ -210,7 +210,7 @@ const ChatPage = ({ socket }) => {
                 <div class="relative flex items-center p-3 border-b border-gray-300">
                   <img
                     class="object-cover w-10 h-10 rounded-full"
-                    src="https://api.lorem.space/image/face?w=150&h=150"
+                    src="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
                     alt="username"
                   />
                   <span class="block ml-2 font-bold text-gray-600">
