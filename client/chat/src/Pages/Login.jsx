eea6d40e176ff5a0a0fca969ee94cdc686857c88
ItVal <valentinhonvj1@gmail.com>
2022-12-10 21:00:25 +0100

    responsive login interface

--- client/chat/src/Pages/Login.jsx
+++ client/chat/src/Pages/Login.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Navbar from "../components/Navbar";
+// import Navbar from "../components/Navbar";
+import logo from "../assets/Chat_Me__1_-removebg-preview.png"
 import "../Styles/login.css";
 import img from "../assets/Chat_Me__1_-removebg-preview.png";
 import { useDispatch, useSelector } from "react-redux";
@@ -65,14 +66,17 @@ const Login = (props) => {
 
   return (
     <>
-      <Navbar />
-      <div className="login relative flex flex-col justify-center min-h-[85vh] overflow-hidden">
-        <div className="w-full p-6 m-auto bg-[#494949] rounded-md shadow-xl lg:max-w-lg">
+      {/* <Navbar /> */}
+      <div className="logo">
+      <img src={logo} alt="logo" />
+      </div>
+      <div className="login relative flex flex-col justify-center mt-0 h-[85vh] overflow-hidden">
+        <div className="cont w-full p-3 ml-auto mb-20 mr-auto bg-[#494949] rounded-md shadow-xl lg:max-w-lg">
           <div className="text-3xl font-semibold text-center text-white uppercase">
             Login
           </div>
 
-          <form className="mt-6">
+          <form className="mt-6 myfor">
             <div className="mb-2">
               <label
                 htmlFor="email"
@@ -82,7 +86,7 @@ const Login = (props) => {
               </label>
               <input
                 type="email"
-                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                className="block w-[90%] px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                 id="email"
                 placeholder="abc@example.com"
                 ref={emailRef}
@@ -97,7 +101,7 @@ const Login = (props) => {
               </label>
               <input
                 type="password"
-                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                className="block w-[90%] px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                 id=""
                 placeholder="Your Password"
                 ref={passwordRef}
@@ -109,7 +113,7 @@ const Login = (props) => {
             <div className="mt-6">
               <button
                 onClick={loginUser}
-                className="w-full px-4 py-2 tracking-wide text-purple-600 font-bold bg-white transition-colors duration-200 transform rounded-md hover:bg-[#40e66a] focus:outline-none focus:bg-purple-600"
+                className="w-[90%] px-4 py-2 tracking-wide text-purple-600 font-bold bg-white transition-colors duration-200 transform rounded-md hover:bg-[#40e66a] focus:outline-none focus:bg-purple-600"
               >
                 Login
               </button>
@@ -118,10 +122,10 @@ const Login = (props) => {
           <div className="relative flex items-center justify-center w-full mt-6 border border-t">
             <div className="absolute px-5 bg-white">Or</div>
           </div>
-          <div className="flex mt-4 gap-x-2">
+          <div className="flex orbtn w-[80%] mt-4 gap-x-2">
             <button
               type="button"
-              className="flex items-center justify-center w-full p-2 border border-gray-[#40e66a] bg-[#03989e] hover:bg-[#40e66a] rounded-md focus:ring-2 focus:ring-offset-1 focus:ring-violet-600"
+              className="flex items-center justify-center w-[70%] p-2 border border-gray-[#40e66a] bg-[#03989e] hover:bg-[#40e66a] rounded-md focus:ring-2 focus:ring-offset-1 focus:ring-violet-600"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -131,7 +135,7 @@ const Login = (props) => {
                 <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
               </svg>
             </button>
-            <button className="flex items-center justify-center w-full p-2 border border-[#40e66a] hover:bg-[#40e66a] rounded-md focus:ring-2 focus:ring-offset-1 focus:ring-violet-600">
+            <button className="flex items-center justify-center w-[70%] p-2 border border-[#40e66a] hover:bg-[#40e66a] rounded-md focus:ring-2 focus:ring-offset-1 focus:ring-violet-600">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 32 32"
@@ -140,7 +144,7 @@ const Login = (props) => {
                 <path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
               </svg>
             </button>
-            <button className="flex items-center justify-center w-full p-2 border border-gray-[#40e66a] hover:bg-[#40e66a] rounded-md focus:ring-2 focus:ring-offset-1 focus:ring-violet-600">
+            <button className="flex items-center justify-center w-[70%] p-2 border border-gray-[#40e66a] hover:bg-[#40e66a] rounded-md focus:ring-2 focus:ring-offset-1 focus:ring-violet-600">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 32 32"
